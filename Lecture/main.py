e9e5cf37db3f83e73113182ddd43994d66ecb26e
96fma <fmartinez@dmacc.edu>
2023-01-28 19:57:39 -0600

    Finished Variable Assignment

--- Lecture/main.py
+++ Lecture/main.py
@@ -1,7 +1,21 @@
-# Variable Assignment
+"""
+Program: Variable Assignment
+Author: Fernando Martinez
+Last date modified: 01/28/2023
 
-x=5
-y=5.2
-v='boat'
-print(type(v))
+The Variable Assignment
+"""
+
+
+# Integer variable
+int_variable = 5
+# Float variable
+float_variable = 5.2
+# String variable
+string_variable = 'boat'
+# Constant variable
+constant = 6.67
+
+print(int_variable)
+print(float_variable)
 
